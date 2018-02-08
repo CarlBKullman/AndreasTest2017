3ea0c48754bb7e0e0578bfd82104736ebe10cbcc
unknown <carl.b.kullman@gmail.com>
2018-02-08 11:28:50 +0100

    refactor YAHOO moch

--- DbConnection/DbConnection.fs
+++ DbConnection/DbConnection.fs
@@ -1,10 +1,17 @@
 ﻿namespace DbConnection
 open Microsoft.FSharp.Data.TypeProviders
 open System.Data.Linq
+
+//DbSchema is used for accessing MS SQL database default database  "Initial Catalog=Andreas" must be created and 
+//login User=Andreas;Password=password must have dbo level access to create tables and manipulate data
+
 type DbSchema = SqlDataConnection<"Data Source=CARL2013\SQLEXPRESS;Initial Catalog=Andreas;User=Andreas;Password=password;">
 
-//type Class1() = 
-//    member this.X = "F#"
+//other constatans
+
+//Properties.MockUrl = "127.0.0.1"
+//Properties.Mockport = 8088
+    
 
 
 
--- FinanceYahooMock/FinanceYahooMockImpl.fs
+++ FinanceYahooMock/FinanceYahooMockImpl.fs
@@ -4,24 +4,28 @@ open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Writers
+open Suave.Successful
 
 [<EntryPoint>]
 let main argv = 
-/// mock for https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1514635129&period2=1517313529&interval=1d&events=history&crumb=z7O4jAW0mv0
-/// MSFT.csv and INTC.csv
+// mock for https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1514635129&period2=1517313529&interval=1d&events=history&crumb=z7O4jAW0mv0
+// MSFT.csv and INTC.csv
+// works
 
   let app : WebPart =
       choose [
         GET >=> path "/" >=> Files.file "index.html"
         GET >=> Files.browseHome
+        //GET >=> path "/v7/finance/download/" >=> Files.browseHome
         RequestErrors.NOT_FOUND "Page not found." 
       ]
   let mimeTypes =
     defaultMimeTypesMap
         @@ (function | ".csv" -> createMimeType " text/csv" false | _ -> None)
   let local = Suave.Http.HttpBinding.createSimple HTTP "127.0.0.1" 8088  
+
   let config = 
-    { defaultConfig with homeFolder = Some (Path.GetFullPath "../../") ; mimeTypesMap = mimeTypes; bindings = [local] }
+    { defaultConfig with homeFolder = Some (Path.GetFullPath "../../webroot") ; mimeTypesMap = mimeTypes; bindings = [local] }
   startWebServer config app
  
   printfn "Make requests now for ex 'http://127.0.0.1:8088/MSFT.csv?...' arguments will be ignored"
